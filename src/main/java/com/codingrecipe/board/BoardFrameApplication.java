dfcae8d36ec9ac484cf94f4e44c5fcf0bf48d777
pack jonghoon <pd8459@gmail.com>
2024-11-24 17:23:33 +0900

    SpringBoot + mysql + mybatis

---
+++ src/main/java/com/codingrecipe/board/BoardFrameApplication.java
@@ -0,0 +1,13 @@
+package com.codingrecipe.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardFrameApplication.class, args);
+    }
+
+}
